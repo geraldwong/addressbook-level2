1daea9c0768a7b3871a4b0ca7c2e00d2938dcadf
Gerald Wong <e0003102@u.nus.edu>
2017-01-31 22:50:36 +0800

    Implement new classes for address

---
+++ src/seedu/addressbook/data/person/PostalCode.java
@@ -0,0 +1,14 @@
+package seedu.addressbook.data.person;
+
+public class PostalCode {
+
+	String postalCode;
+	
+	public PostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	
+	public String getPostalCode() {
+		return this.postalCode;
+	}
+}
